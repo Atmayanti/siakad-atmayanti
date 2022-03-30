92faa080c7c7c17c0d984865dc55e8ed62b7d5ff
Atmayanti <mayablue74@gmail.com>
2022-03-30 21:17:09 +0700

    Praktikum Jobsheet 7

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model; //Model Eloquent
+
+class Mahasiswa extends Model
+{
+    protected $table='mahasiswa';//Eloquent akan membuat model mahasiswa untuk menyimpan record
+    protected $primaryKey = 'id_mahasiswa'; // Memanggi; isi DB dengan primarykey
+
+    protected $fillable = [
+        'Nim',
+        'Nama',
+        'Kelas',
+        'Jurusan',
+    ];
+}
